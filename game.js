b96b10070400bbbc798650231804eb166367cc5d
justin-ivancic <justin.ivancic@gmail.com>
2025-06-04 11:50:03 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-
+const CROP_DATA = {
+  wheat:      { seedCost: 5,  harvestPrice: 12, growthTime: 9 },  // 9 seconds
+  corn:       { seedCost: 8,  harvestPrice: 18, growthTime: 12 }, // 12 seconds
+  tomato:     { seedCost: 10, harvestPrice: 22, growthTime: 15 }, // 15 seconds
+  carrot:     { seedCost: 6,  harvestPrice: 14, growthTime: 8  }, //  8 seconds
+  flower:     { seedCost: 4,  harvestPrice: 10, growthTime: 6  }, //  6 seconds
+  glowshroom: { seedCost: 12, harvestPrice: 30, growthTime: 18 }  // 18 seconds
+};
